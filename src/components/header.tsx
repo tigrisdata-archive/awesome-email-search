f580a6c9a9311a03ea1cc69a3d46e482169652e5
Phil Leggetter <phil@leggetter.co.uk>
2023-05-23 12:27:41 +0100

    chore: add logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,7 +22,17 @@ export default function Header() {
     <header className="w-full max-w-5xl">
       <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <Link href="/">Awesome Email Search</Link>
+          <Link className="flex gap-2 items-center" href="/">
+            <Image
+              src="/email.svg"
+              alt="Email Logo"
+              className="dark:invert"
+              width={24}
+              height={24}
+              priority
+            />
+            <span>Awesome Email Search</span>
+          </Link>
         </p>
         <div className="fixed bottom-0 left-0 flex w-full items-start justify-center lg:justify-start py-4 lg:py-0 lg:static h-auto lg:w-auto bg-gray-950 lg:bg-none z-10">
           <span className="flex self-start">With</span>
