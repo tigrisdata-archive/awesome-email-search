7b3e185794a8eaff4c58af8d1a3af9fa44e76186
Phil Leggetter <phil@leggetter.co.uk>
2023-05-23 16:41:42 +0100

    chore: fix heading z-index

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,8 +19,8 @@ export default function Header() {
   const pathname = usePathname();
 
   return (
-    <header className="w-full max-w-5xl">
-      <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
+    <header className="w-full max-w-5xl z-20">
+      <div className="w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <Link className="flex gap-2 items-center" href="/">
             <Image
