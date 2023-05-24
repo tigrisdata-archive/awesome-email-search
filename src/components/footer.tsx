d7d03bfc850986b3148b2d41259670163e62873b
Phil Leggetter <phil@leggetter.co.uk>
2023-05-24 02:10:25 +0100

    feat: add link to codebase

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,23 @@
+import Link from 'next/link';
 import Row from './row';
+import Image from 'next/image';
 
 export default function Footer() {
-  return <Row></Row>;
+  return (
+    <Row>
+      <Link
+        className="flex items-center gap-2"
+        href="https://github.com/tigrisdata-community/awesome-email-search"
+      >
+        <Image
+          src="/github.svg"
+          alt="GitHub logo"
+          width={24}
+          height={24}
+          className="dark:invert"
+        />{' '}
+        Grab the code on GitHub
+      </Link>
+    </Row>
+  );
 }
