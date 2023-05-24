8186f2887abc33ee7e121019d72b97808df913cc
Phil Leggetter <phil@leggetter.co.uk>
2023-05-24 22:37:22 +0100

    feat: font-size bump + homepage gradient

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
 
   return (
     <header className="w-full max-w-5xl z-20">
-      <div className="w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="w-full items-center justify-between text-xl lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <Link className="flex gap-2 items-center" href="/">
             <Image
