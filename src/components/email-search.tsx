5cf1815906ed03ecb671811e2e77add530a7a34f
Phil Leggetter <phil@leggetter.co.uk>
2023-05-24 20:37:21 +0100

    chore: remove search button

--- src/components/email-search.tsx
+++ src/components/email-search.tsx
@@ -166,12 +166,6 @@ export const EmailSearch = (props: EmailSearchProps) => {
             isNaN(Number(status))
           )}
         />
-        <button
-          disabled={searching}
-          className="flex-grow lg:grow-0 h-10 lg:h-auto self-stretch lg:w-36 px-4 font-semibold text-sm bg-green-50 dark:bg-gray-800 text-white rounded-md shadow-sm opacity-100 hover:bg-green-300 hover:dark:bg-gray-600"
-        >
-          {searching ? 'Searching...' : 'Search'}
-        </button>
       </form>
 
       <div className="mt-10 relative overflow-x-scroll shadow-md sm:rounded-lg  w-full max-w-5xl mx-4">
